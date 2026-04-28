c0f34203a94811fe1b7ae4a86e54a10f45ac7515
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-04-29 02:58:03 +0300

    add eraser button and paint button

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let gridSize = 16;
-let draw = false;
 let isMouseDown = false;
+let currentMode = "paint";
 
 const container = document.querySelector('.container');
 const gridArea = document.querySelector('.middle-place');
 const buttons = document.querySelectorAll('.btn');
-const paintBtn = document.querySelector('#paint-btn');
-const rainbowBtn = document.querySelector('#rainbow-btn');
-const eraserBtn = document.querySelector('#eraser-btn');
-const clearBtn = document.querySelector('#clear-btn');
-const hideBtn = document.querySelector('#hide-btn');
-const colorPicker = document.querySelector('.color-picker');
-const slider = document.querySelector('.slider');
+const paintBtn = document.querySelector('.paint-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const eraserBtn = document.querySelector('.eraser-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const hideBtn = document.querySelector('.hide-btn');
+const colorPicker = document.querySelector('#color-picker');
+const slider = document.querySelector('#slider');
 const gridValue = document.querySelector('.grid-value');
 
 document.addEventListener('mousedown', () => {
@@ -24,12 +23,20 @@ document.addEventListener('mouseup', () => {
 
 function paintCell(e) {
     if (e.type === "mousedown" || isMouseDown) {
-        e.target.style.backgroundColor = colorPicker.value;
+
+        if (currentMode === "paint") {
+            e.target.style.backgroundColor = colorPicker.value;
+        }
+
+        if (currentMode === "eraser") {
+            e.target.style.backgroundColor = "#ffffff";
+        }
     }
 }
 
-function createGrid(){
-    let size = Number(slider.value);
+function createGrid(size){
+
+    size = Number(slider.value);
 
     gridArea.innerHTML = "";
 
@@ -47,5 +54,20 @@ function createGrid(){
     }
 }
 
+slider.addEventListener('input', () => {
+    let size = Number(slider.value);
+
+    gridValue.textContent = `${size} x ${size}`;
+
+    createGrid(size);
+});
+
+eraserBtn.addEventListener('click', () => {
+    currentMode = "eraser";
+})
+
+paintBtn.addEventListener('click', () => {
+    currentMode = "paint";
+})
 
 createGrid();
